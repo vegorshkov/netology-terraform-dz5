f2429cea52848b97489c3c84eff4d6635b6d96db
Горшков Виктор Евгеньевич <vgorshkov@gid.ru>
2026-02-22 11:23:29 +0300

    task4

--- variables.tf
+++ variables.tf
@@ -19,20 +19,8 @@ variable "default_zone" {
   default     = "ru-central1-a"
   description = "https://cloud.yandex.ru/docs/overview/concepts/geo-scope"
 }
-# variable "default_cidr" {
-#   type        = list(string)
-#   default     = ["10.0.1.0/24"]
-#   description = "https://cloud.yandex.ru/docs/vpc/operations/subnet-create"
-# }
-
-# variable "vpc_name" {
-#   type        = string
-#   default     = "develop"
-#   description = "VPC network&subnet name"
-# }
 
 ###common vars
-
 variable "vms_ssh_root_key" {
   type        = string
   description = "ssh-keygen -t ed25519"
@@ -44,13 +32,3 @@ variable "vm_web_name" {
   default     = "netology-develop-platform-web"
   description = "example vm_web_ prefix"
 }
-
-###example vm_db var
-# variable "vm_db_name" {
-#   type        = string
-#   default     = "netology-develop-platform-db"
-#   description = "example vm_db_ prefix"
-# }
-
-
-
